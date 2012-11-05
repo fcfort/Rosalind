618eac7ffd9c0eb5ce069f84672e9d1cebaaa9d9
ffdYKJisu <phranck+github@gmail.com>
2012-11-05 07:19:41 -0500

    more complete but still incomplete soln to rear

--- rear/main.pl
+++ rear/main.pl
@@ -3,12 +3,14 @@ use strict;
 use warnings;
 use lib '../common';
 use Common;
+use Text::Levenshtein::Damerau::XS qw/xs_edistance/;
 
 # Create char arrays
 my @orig = get_int_string(split / /, Common::read_line());
 my @dest = get_int_string(split / /, Common::read_line());
 
-rev_search(\@orig, \@dest, 0);
+my $min_depth = rev_search(\@orig, \@dest, 0);
+print "Got a min depth of $min_depth\n";
 
 sub rev_search { 
     my ($orig, $dest) = @_;
@@ -41,23 +43,41 @@ sub rev_search_helper {
     for(my $i = 0; $i < $G::len - 1; $i++) {
         for(my $j = $i + 1; $j < $G::len; $j++) {
             my @proposal = @$current;
-            @proposal[$i..$j] = reverse @proposal[$i..$j];
-            print "Flipping $i to $j: " .
-                join_array(@$current) . " -> " . join_array(@proposal) . "\n";
+            @proposal[$i..$j] = reverse @$current[$i..$j];
+            # print "Flipping $i to $j: " .
+            #    join_array(@$current) . " -> " . join_array(@proposal) . "\n";
             if ( Common::hamming_distance(
                     join_array(@proposal), $G::orig_str )
                 <
                  Common::hamming_distance(
                     join_array(@$current), $G::orig_str )
                  ) {
-                if ( $depth < $min_depth ) {
-                print "Must go deeper, recursing with " .
-                    join_array(@proposal), "\n";
-                
-                rev_search_helper(\@proposal, $depth + 1);
+                if ( $cur_depth < $min_depth ) {
+                    print "Got HD( " . Common::hamming_distance(
+                    join_array(@proposal), $G::orig_str ) . " ) < HD ( " .
+                    Common::hamming_distance(
+                    join_array(@$current), $G::orig_str ) . " ) " , "\n";
+
+
+                    print "Must go deeper, recursing with " .
+                        join_array(@proposal), "\n";
+                    
+                    my $depth_reached = rev_search_helper(\@proposal, $cur_depth + 1, $min_depth);
+                    if ( $depth_reached < $min_depth ) {
+                        $min_depth = $depth_reached;
+                    }
+                }
             }
         }
     }
+
+    return $min_depth;
+}
+
+# distance
+sub distance {
+    my ($str1, $str2) = @_;
+    return xs_distance($str1, $str2);
 }
 
 # Convert numbers to strings
