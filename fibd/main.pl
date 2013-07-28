619783f3304be5b119860a6fd37aacd0ed286cd2
Frank <phranck+c9@gmail.com>
2013-07-28 13:14:05 -0400

    solved fibd

--- fibd/main.pl
+++ fibd/main.pl
@@ -6,35 +6,45 @@ use Common;
 use Data::Dumper;
 
 # n months
-# k rabbit offspring
-my ($n, $k) = split / /, Common::read_line();
+# m rabbit lifespan
+my ($n, $m) = split / /, Common::read_line();
 
-# Recurrence relation for k offspring
-# F(n) = F(n-1)  + F(n-2)*k
-
+# The number of rabbits after any generation F(n)) is the
+# sum of the number of young rabbits (G(n)) and old 
+# rabbits (H(n)).
+# F(n) = G(n) + H(n)
 
 my %g = (
-    0 => 1,
-    1 => 0,   
+    1 => 1,
+    2 => 0,   
 );
 my %h = (
-    0 => 0,
-    1 => 1,
+    1 => 0,
+    2 => 1,
 );
 my %f = (
-    0 => 1,
-    1 => 1,
+    1 => $g{1} + $h{1},
+    2 => $g{2} + $h{2},
 );
+# print Dumper(\%f);
+# YOUNG : Each old rabbit produces 1 young rabbit
+# G(n) = H(n-1)
 
-print Dumper(\%g);
+# OLD : Each old rabbit alive in the previous generation lives on
+# Each young rabbit becomes and old rabbit
+# ago die out
+# H(n) = H(n-1) + G(n-1) - G(n-m+1)
+# m = 3 : H(n) = H(n-1) + G(n-1) - G(n-2)
 
-
-for(2..$n) {
-    $g{$_} = $h{$_-1};
-    $h{$_} = $g{$_-1} + $g{$_-2};
-    $f{$_} = $g{$_} + $h{$_};
-    print "@ $n : F = $f{$_}, G = $g{$_}, H = $h{$_} \n";
+for(3..$n) {
+    $g{$_} = h($_-1);
+    $h{$_} = h($_-1) + g($_-1) - g($_-$m);
+    $f{$_} = g($_) + h($_);
+    print "n = $_ : F = $f{$_}, G = $g{$_}, H = $h{$_} \n";
 }
 
 print "Answer is $f{$n}\n";
 
+# shortcut functions
+sub h { return $h{$_[0]} // 0; }
+sub g { return $g{$_[0]} // 0; }
