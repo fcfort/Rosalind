17576b4697d412f1a9db2c465d53da50366a0963
Frank <phranck+c9@gmail.com>
2013-07-29 17:23:02 -0400

    added LCS algorithm code to script

--- long/main.pl
+++ long/main.pl
@@ -10,7 +10,12 @@ while( defined ( my $line = Common::read_line() ) ) {
     push(@dna, $line);
 }
 
+
 # print Dumper(\@dna);
+for(@dna) {
+    
+}
+lc_substr
 
 # To record a pair, we need to know 
 my @joins;
@@ -70,4 +75,44 @@ sub half_length_match {
     
     print "Front $a ($front) equal to back $b ($back)\n";
     return $front eq $back;
+}
+
+# From http://en.wikibooks.org/wiki/Algorithm_implementation/Strings/Longest_common_substring#Perl
+sub lc_substr {
+  my ($str1, $str2) = @_; 
+  my $l_length = 0; # length of longest common substring
+  my $len1 = length $str1; 
+  my $len2 = length $str2; 
+  my @char1 = (undef, split(//, $str1)); # $str1 as array of chars, indexed from 1
+  my @char2 = (undef, split(//, $str2)); # $str2 as array of chars, indexed from 1
+  my @lc_suffix; # "longest common suffix" table
+  my @substrings; # list of common substrings of length $l_length
+ 
+  for my $n1 ( 1 .. $len1 ) { 
+    for my $n2 ( 1 .. $len2 ) { 
+      if ($char1[$n1] eq $char2[$n2]) {
+        # We have found a matching character. Is this the first matching character, or a
+        # continuation of previous matching characters? If the former, then the length of
+        # the previous matching portion is undefined; set to zero.
+        $lc_suffix[$n1-1][$n2-1] ||= 0;
+        # In either case, declare the match to be one character longer than the match of
+        # characters preceding this character.
+        $lc_suffix[$n1][$n2] = $lc_suffix[$n1-1][$n2-1] + 1;
+        # If the resulting substring is longer than our previously recorded max length ...
+        if ($lc_suffix[$n1][$n2] > $l_length) {
+          # ... we record its length as our new max length ...
+          $l_length = $lc_suffix[$n1][$n2];
+          # ... and clear our result list of shorter substrings.
+          @substrings = ();
+        }
+        # If this substring is equal to our longest ...
+        if ($lc_suffix[$n1][$n2] == $l_length) {
+          # ... add it to our list of solutions.
+          push @substrings, substr($str1, ($n1-$l_length), $l_length);
+        }
+      }
+    }
+  }   
+ 
+  return @substrings;
 }
